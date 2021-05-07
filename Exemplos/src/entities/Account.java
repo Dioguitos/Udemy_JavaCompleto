949265c7e518ac7811b0420424b87f4e1da37a12
Diogo Pereira <diogo.jpereira@hotmail.com>
2021-05-06 22:16:24 -0300

    Conceitos Herança

---
+++ Exemplos/src/entities/Account.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Account {
+	protected Integer number;
+	protected String holder;
+	protected Double balance;
+	
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public Account() {
+	}
+	
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+}
